f8f877e8577c29a5b712dd6a70cf86c396f6aa9e
RiniPuspitaWati <112569358+RiniPuspitaWati@users.noreply.github.com>
2023-02-22 09:04:02 +0700

    Add files via upload

---
+++ Introduce Your Self rini.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main (){
+ string r;
+ cout<<"Nama Lengkap : ";
+ getline (cin,r);
+ 
+ string i;
+ cout<<"Kelas : ";
+ getline(cin,i);
+ 
+ string n;
+ cout<<"Input Nilai DDP : ";
+ getline (cin,n);  
+ 
+ 	cout<<"--------------------------------"<<endl;
+    cout<<r<<endl<<i<<endl<<n;
+    
+ return 0;
+}
